13b8bdb69e56fba18583f824c0d374d572d8922f
abdoharib <abdoamazon9@gmail.com>
2023-07-02 12:42:19 +0200

    base

---
+++ assets/js/firebase-notification-init.js
@@ -0,0 +1,33 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getMessaging, onMessage, getToken } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA8zhwpjIYV1NY2ccesPK37MKA1wBdFgjA",
+    authDomain: "deepstore-327da.firebaseapp.com",
+    projectId: "deepstore-327da",
+    storageBucket: "deepstore-327da.appspot.com",
+    messagingSenderId: "640699131725",
+    appId: "1:640699131725:web:7a606614c00c56dc16f904"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+Notification
+    .requestPermission()
+    .then(function () {
+        console.log("Notification permission granted.");
+        return getToken(messaging, { vapidKey: 'BK0INhjt8HhZqLCs6vjk1seBLCmEp3MPpsqaYK5ewqJYbrzEw5NPyPFEvCHhgoNZTSzeOanu6s3tc2j8bXDibq8' })
+
+    }).then(function (token) {
+        localStorage.setItem('fcm', token);
+    })
+    .catch(function (err) {
+        console.log("Unable to get permission to notify.", err);
+    });
---
+++ axios.js
@@ -0,0 +1,22 @@
+
+const token = localStorage.getItem('token');
+
+axios.interceptors.response.use(function (response) {
+  // Do something with response data
+  if(response.status == 401){
+    localStorage.clear();
+    window.location.replace('app-login.html')
+  }
+  return response;
+}, function (error) {
+  // Do something with response error
+  return Promise.reject(error);
+});
+var api = axios.create({
+  baseURL: 'http://deep_store.test/delivery_api/',
+  headers: {'Authorization': 'Bearer ' + token },
+})
+
+
+
+export default api
